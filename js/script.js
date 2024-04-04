5ada2288db2c91e4a851633685bccc77f0fcefaa
MumladzeT <tatiamumladze2004@gmail.com>
2024-04-04 17:50:58 +0400

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,46 @@
+let X = 100;
+if (X < 50) {
+    result = "X ნაკლებია 50 ზე"
+}
+
+else {
+    result = "შეცდომა"
+}
+console.log(result)
+
+if (X > 20) {
+    result = " X მეტია 20 ზე "
+}
+else {
+    result = "შეცდომა"
+}
+console.log(result)
+
+
+
+let Y = "მარიამი"
+if (Y = "მარიამი") {
+    console.log(ture);
+}
+
+else {
+    console.log(false);
+}
+
+
+
+let a = "მარიამ"
+switch (a) {
+    case 0:
+        if (a = "მარიამი") {
+            console.log(true);
+        }
+
+        else {
+            console.log(false);
+        }
+        break;
+}
+
+let number=10;
+console.log(String(number));
